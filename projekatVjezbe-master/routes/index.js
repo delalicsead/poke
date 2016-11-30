f29464b781fdadf3ba354e7bee2e9c841a5a7135
delalicsead <delalic.sead@gmail.com>
2016-12-01 00:36:45 +0100

    Usmjeravanje

---
+++ projekatVjezbe-master/routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var pokemons = require('../pokemon.json');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    console.info("Login");
+    res.render('login', {title: 'Sejo'});
+});
+
+router.get('/mapa', function (req, res, next) {
+    console.info("Mapa");
+    res.render('index', {title: 'Index', value: 'Sead'});
+})
+
+router.get('/dajBroj', function (req, res, next) {
+    var sjever = 43.865506;
+    var jug = 43.839387;
+    var istok = 18.443676;
+    var zapad = 18.348404;
+    var poke = (Math.random() * 4 + 1) >>> 0;
+
+    res.send({
+            pokemons: pokemons[poke].name,
+            //lat: 43.850282 + Math.random() * 0.03,
+            //lon: 18.392092 + Math.random() * 0.03
+            lat: Math.random() * (jug - sjever) + sjever,
+            lon: Math.random() * (zapad - istok) + istok
+        }
+    )
+});
+
+
+module.exports = router;
